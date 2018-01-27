2b7cfb001d39865910d9ea61710070eeff685330
Dave Snowberger <git@fartherout.org>
2018-01-27 15:54:20 -0800

    added models for thread and comment

--- public/scripts/models/thread.js
+++ public/scripts/models/thread.js
@@ -1,3 +1,32 @@
-$(function() {
-    console.log('dom ready!');
-});
+'use strict';
+var app = app || {};
+
+(function(module) {
+  var Thread.comments = [];
+  function Thread(obj) {
+    for (let prop in obj) this[prop] = obj[prop];
+  }
+
+  Thread.prototype.fetchComments = function(ctx,next) {
+    $.ajax({
+      url: `${__API_URL__}/api/db/thread/${this.id}`,
+      method: 'GET',
+    })
+    .then(results => {
+      ctx.results = results;
+      next();
+    });
+  }
+
+  Thread.prototype.loadComments = function(ctx,next) {
+    this.comments = ctx.results.map(comment => new Comment(comment));
+    next();
+  }
+
+  Thread.prototype.render = function(ctx,next) {
+    let $threadView = $('.threadView');
+    this.comments.forEach(comment => $threadView.append(comment.toHtml()));
+    next();
+  }
+
+})(app);
