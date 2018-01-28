8f62c0d71442da7a46f035b5f96486090c1f6d8e
Dave Snowberger <git@fartherout.org>
2018-01-27 17:04:37 -0800

    fixed bugs on thread model

--- public/scripts/models/thread.js
+++ public/scripts/models/thread.js
@@ -11,21 +11,21 @@ var app = app || {};
     $.ajax({
       url: `${__API_URL__}/api/db/thread/${this.id}`,
       method: 'GET',
-    })
-    .then(results => {
-      ctx.results = results;
-      next();
+      success: results => {
+        ctx.results = results;
+        next();
+      }
     });
   }
 
   Thread.prototype.loadComments = function(ctx,next) {
-    this.comments = ctx.results.map(comment => new Comment(comment));
+    Thread.comments = ctx.results.map(comment => new app.Comment(comment));
     next();
   }
 
   Thread.prototype.render = function(ctx,next) {
     let $threadView = $('.threadView');
-    this.comments.forEach(comment => $threadView.append(comment.toHtml()));
+    Thread.comments.forEach(comment => $threadView.append(comment.toHtml()));
     next();
   }
 
