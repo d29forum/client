58b25ceb7a2c2216bd117c448f0f9619a226eb62
Dave Snowberger <git@fartherout.org>
2018-01-28 12:21:05 -0800

    added csr for subforum and thread views

--- public/scripts/controllers/routes.js
+++ public/scripts/controllers/routes.js
@@ -5,6 +5,8 @@ page('/login', app.loginUserView.init);
 page('/signup', app.newUserView.init);
 page('/user/:username/edit', app.editUserView.init);
 page('/user/:username', app.userView.init);
+page('/subfora/:id', app.Subforum.prototype.fetchThreads, app.Subforum.prototype.loadThreads, app.Subforum.renderThreads, app.subforumView.init);
+page('/subfora/:subforum_id/threads/:thread_id', app.Thread.prototype.fetchComments, app.Thread.prototype.loadComments, app.Thread.prototype.renderComments, app.threadView.init);
 page('/*', app.errorView.init);
 page('/*/*', app.errorView.init);
 page('/*/*/*', app.errorView.init);
