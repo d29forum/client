7eec6becaba45c0228665dc9082505a6dc4a4385
Brian Bixby <brianbixby0@gmail.com>
2018-01-30 11:08:57 -0800

    user loginupdates time stamp

--- public/scripts/models/user.js
+++ public/scripts/models/user.js
@@ -62,42 +62,22 @@ const __API_URL__ = 'http://localhost:3737';
       });
   }
 
-  // User.login = function(user) {
-  //   console.log(user);
-  //   $.ajax({
-  //     url: `${__API_URL__}/api/db/users/${user.username}`,
-  //     method: 'GET',
-  //     success:(results => {
-  //       console.log(results);
-  //       if(!results[0]) {
-  //         User.userIdNotFound(user.username);
-  //       }
-  //       else if (results[0].username == user.username){
-  //         localStorage.currentUserId = results[0].id;
-  //         localStorage.currentUserName = results[0].username;
-  //         window.location = '../';
-  //       }
-  //     })
-  //     //error: app.errorView.init,
-  //   });
-  // }
   User.login = function(user) {
     console.log(user);
     $.ajax({
-      url: `${__API_URL__}/api/db/users/${user.username}/login`,
-      method: 'PUT',
+      url: `${__API_URL__}/api/db/users/${user.username}`,
+      method: 'GET',
       success:(results => {
         console.log(results);
-        console.log(res.status);
-        // if(!results[0]) {
-        //   User.userIdNotFound(user.username);
-        // }
-        // else if (results[0].username == user.username){
-        //   localStorage.currentUserId = results[0].id;
-        //   localStorage.currentUserName = results[0].username;
-        //   window.location = '../';
-        // }
-      }),
+        if(!results[0]) {
+          User.userIdNotFound(user.username);
+        }
+        else if (results[0].username == user.username){
+          localStorage.currentUserId = results[0].id;
+          localStorage.currentUserName = results[0].username;
+          window.location = '../';
+        }
+      })
       //error: app.errorView.init,
     });
   }
