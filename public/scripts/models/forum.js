40c8dd27f9864668b5bb2538d393de0bb6e2862c
Brian Bixby <brianbixby0@gmail.com>
2018-01-29 19:46:58 -0800

    forum view works

--- public/scripts/models/forum.js
+++ public/scripts/models/forum.js
@@ -2,23 +2,46 @@
 var app = app || {};
 
 (function(module) {
-  function Subforum(obj) {
-    for (let prop in obj) this[prop] = obj[prop];
+  function Forum(forumObj) {
+    for (let prop in forumObj) this[prop] = forumObj[prop];
   }
 
-  Forum.subforums = [];
+  Forum.all = [];
 
-  // Forum.prototype.insert = function(ctx,next) {
-  //   $.ajax({
-  //     url: `${__API_URL__}/api/db/forum`,
-  //     method: 'POST',
-  //     data: {title: this.title, subtitle: this.subtitle},
-  //     success: results => {
-  //       ctx.results = results;
-  //       next();
-  //     }
-  //   });
-  // }
+  // handlebars template for user profile
+  Forum.prototype.toForumTemplateHtml = function() {
+    var template = Handlebars.compile($('#forum-template').text());
+    console.log(template(this));
+    return template(this);
+  }
+
+  // 3rd - maps user from constructor to tamplate and appends it to html
+  Forum.renderCurrent = (ctx, next) => {
+      console.log('render');
+      $('#subforaContainer').empty();
+      Forum.all.map(forum => $('#subforaContainer').append(forum.toForumTemplateHtml()));
+      // $('#editProfileButton').attr('href', `/user/${ctx.params.username}/edit`)
+      // next();
+  }
+
+  // 2ND - takes the individual result and maps it to  the new User constructor
+  Forum.loadCurrent = (ctx, next) => {
+      Forum.all = ctx.results.map(forumObj => new Forum(forumObj));
+      console.log(Forum.all);
+      next();
+  }
+
+  Forum.fetchSubfora = function(ctx,next) {
+    $.ajax({
+      url: `${__API_URL__}/api/db/forum`,
+      method: 'GET',
+      success: results => {
+        console.log(results);
+        ctx.results = results;
+        next();
+      }
+    });
+  }
 
   module.Forum = Forum;
 })(app);
