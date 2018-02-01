10498d486adc90a0ef2611d1c8d5afa55cb34b14
Brian Bixby <brianbixby0@gmail.com>
2018-01-31 22:58:33 -0800

    fixed forum view toggles when you come back to page among other minor bugs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -27,21 +27,13 @@ $('#hamburgerToggle a').on('click', function() {
     $('.userContainer').toggleClass('slideOut');
 });
 
-// HOME ACCORDION
-
-$('.subforaContainer').on('click', '.accordionToggle', function() { 
-    if($(this).parent().find('.lastPostRow').hasClass('randomClass')) {
-        $(this).parent().find('.lastPostRow').removeClass('randomClass').addClass('hidden').slideToggle(500);
-    }
-    else {
-        $('.subforaContainer').find('.randomClass').removeClass('randomClass').addClass('hidden').slideToggle(500);
-        $(this).parent().find('.lastPostRow').addClass('randomClass').slideToggle(500).removeClass('hidden');
-    }
-    if($(this).hasClass('openAccordion')) {
-        $(this).removeClass('openAccordion');
-    }
-    else {
-        $('.subforaContainer').find('.openAccordion').removeClass('openAccordion');
-        $(this).addClass('openAccordion');
+// HIDE DROPDOWN WHEN YOU CLICK HOME
+$('#logo').on('click', function() {
+    if($('#hamburgerToggle').css('display')=='block') {
+        $('.flipping').toggleClass('flip');
+        $('.hamburger').toggleClass('open');
+        $('#hamburgerToggle').slideToggle(500);
+        $('nav').toggleClass('darkNav');
+        $('.userContainer').toggleClass('slideOut');
     }
 });
--- public/scripts/views/forum/forumView.js
+++ public/scripts/views/forum/forumView.js
@@ -7,7 +7,25 @@ var app = app || {};
     forumView.init = function(ctx, next) {
         $('.view').addClass('hidden').find('*').off();
         $('.forumView').removeClass('hidden');
+        $('.subforaContainer').on('click', '.accordionToggle', forumView.accordionControls);
         next();
+    }
+
+    forumView.accordionControls = function() { 
+        if($(this).parent().find('.lastPostRow').hasClass('randomClass')) {
+            $(this).parent().find('.lastPostRow').removeClass('randomClass').addClass('hidden').slideToggle(500);
+        }
+        else {
+            $('.subforaContainer').find('.randomClass').removeClass('randomClass').addClass('hidden').slideToggle(500);
+            $(this).parent().find('.lastPostRow').addClass('randomClass').slideToggle(500).removeClass('hidden');
+        }
+        if($(this).hasClass('openAccordion')) {
+            $(this).removeClass('openAccordion');
+        }
+        else {
+            $('.subforaContainer').find('.openAccordion').removeClass('openAccordion');
+            $(this).addClass('openAccordion');
+        }
     }
     
     module.forumView = forumView;
